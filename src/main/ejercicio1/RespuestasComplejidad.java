8c8d2158337dc4987514187d9e8ef1bc16d46a55
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-05-20 08:37:32 -0500

    Ejercicio1_Corregido

--- src/main/ejercicio1/RespuestasComplejidad.java
+++ src/main/ejercicio1/RespuestasComplejidad.java
@@ -2,14 +2,14 @@ package main.ejercicio1;
 
 public class RespuestasComplejidad {
     // Los estudiantes deben completar cada variable con la notación Big O correcta
-    public static String imprimirArreglo = "O(n)";
-    public static String busquedaLineal = "O(n)";
-    public static String busquedaBinaria = "O(log n)";
-    public static String factorial = "O(n)";
-    public static String fibonacci = "O(2^n)";
-    public static String bubbleSort = "O(n^2)";
-    public static String quickSort = "O(n log n)";
-    public static String sumaMatriz = "O(n^2)";
-    public static String encontrarMaximo = "O(n)";
-    public static String potencia = "O(n)";
+    public static String imprimirArreglo = "O(n)"; // 1
+    public static String busquedaLineal = "O(n)"; // 2
+    public static String busquedaBinaria = "O(log n)"; // 3
+    public static String factorial = "O(n)"; // 4
+    public static String fibonacci = "O(2^n)"; // 5
+    public static String bubbleSort = "O(n^2)"; // 6
+    public static String quickSort = "O(n log n)"; // 7
+    public static String sumaMatriz = "O(n^2)"; // 8
+    public static String encontrarMaximo = "O(n)"; // 9
+    public static String potencia = "O(n)"; // 10
 }
